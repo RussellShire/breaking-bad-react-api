8e5e2313c394fbd0d99bf007e17656a603bbd22c
RussellShire <russell.shire@gmail.com>
2022-09-07 13:33:58 +0100

    Add cards visualisation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import CharacterGrid from './components/characters/CharacterGrid';
+import Header from './components/ui/Header'
 import './App.css';
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+      
+      // console.log(result.data)
+
+      setItems(result.data)
+      setIsLoading(false)
+    }
+
+    fetchItems()
+  }, [])
+
   return (
     <div className='container'>
-      hello
+      <Header />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 }
