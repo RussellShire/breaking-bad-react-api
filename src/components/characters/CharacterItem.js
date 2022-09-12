6fec5430ad6bf2c388206d171b31c5ca17e5bdfb
RussellShire <russell.shire@gmail.com>
2022-09-12 13:51:06 +0100

    Remove chuff

--- src/components/characters/CharacterItem.js
+++ src/components/characters/CharacterItem.js
@@ -2,9 +2,6 @@ import React from 'react';
 
 const CharacterItem = ({ item }) => {
     return (
-        // <div>
-        //     {item.name}
-        // </div>
         <div className='card'>
             <div className='card-inner'>
                 <div className='card-front'>
